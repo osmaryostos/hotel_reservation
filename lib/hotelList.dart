f3cf5710a6b6d6d4b39fdd774f8519558254bad0
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2023-06-27 12:08:11 -0400

    Added scrolling, divider, and navigation. Added font to pubspec.

--- lib/hotelList.dart
+++ lib/hotelList.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'GRDetails.dart';
+import 'RSDetails.dart';
 
 void main() => runApp(const HotelList());
 
@@ -16,7 +18,6 @@ final logomono = Image.asset(
   'images/logomono.png',
   fit: BoxFit.cover,
 );
-
 
 var stars = Row(
   mainAxisSize: MainAxisSize.min,
@@ -42,6 +43,7 @@ var stars2 = Row(
 
 class HotelList extends StatelessWidget {
   const HotelList({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -59,68 +61,82 @@ class HotelList extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('Welcome!'),
-            Text('Please, choose one of our hotels.'),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children:[
-                  Text('GRAND ROYAL'),
-                  stars,
-                ]
-            ),
-            royalsuites,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text('C\$ 390,00'),
-                ElevatedButton(
-                  onPressed: (){
-                    //Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WelcomePage(name: _name.text, email: _email.text, phone: _phone.text)));
-                  },
-                  child: Text('Select Hotel'),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                    backgroundColor: Colors.black,
-                    padding: EdgeInsets.all(10),
-                  ),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child:
+                  Text('Welcome!', style: TextStyle(fontSize: 18,),), // + name),
                 ),
-
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child:
+                  Text('Please, choose one of our hotels.', style: TextStyle(fontSize: 18,),),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children:[
+                      Text('GRAND ROYAL',style: TextStyle(fontSize: 16,),),
+                      stars,
+                    ]
+                ),
+                royalsuites,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('C\$ 390,00',style: TextStyle(fontSize: 16,),),
+                    ElevatedButton(
+                      onPressed: (){
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailsGR()));
+                      },
+                      child: Text('Select Hotel'),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        backgroundColor: Colors.black,
+                        padding: EdgeInsets.all(10),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  height: 60,
+                  thickness: 1,
+                  color: Colors.grey,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children:[
+                      Text('ROYAL SUITES',style: TextStyle(fontSize: 16,),),
+                      stars2,
+                    ]
+                ),
+                grandroyal,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('C\$ 150,00',style: TextStyle(fontSize: 16,),),
+                    ElevatedButton(
+                      onPressed: (){
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> RSDetails()));
+                      },
+                      child: Text('Select Hotel'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.black),
+                        padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            Text(' '),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children:[
-                  Text('ROYAL SUITES'),
-                  stars2,
-                ]
-            ),
-            grandroyal,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('C\$ 150,00'),
-                ElevatedButton(
-                  onPressed: (){
-                    //Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WelcomePage(name: _name.text, email: _email.text, phone: _phone.text)));
-                  },
-                  child: Text('Select Hotel'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(10)),
-                  ),
-                ),
-              ],
-            ),
-
-          ],
+          ),
         ),
       ),
     );
-
-
   }
 }
